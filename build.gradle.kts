5340d7c9d268d78be1a2925f74e47a86300ea119
amit goel <amit.goel@wildhealth.com>
2023-12-06 09:59:25 -0500

    Add kover and ktlint plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,23 @@ plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
+    alias(libs.plugins.ktlint)
+    alias(libs.plugins.kover)
+}
+
+subprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+    apply(plugin = "org.jetbrains.kotlinx.kover")
+
+    kover {}
+
+    // Optionally configure plugin
+    configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
+        verbose.set(true)
+        outputToConsole.set(true)
+
+        filter {
+            exclude { it.file.path.contains("build/") }
+        }
+    }
 }
