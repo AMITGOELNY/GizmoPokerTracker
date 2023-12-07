e07938073483b2704e76b54600cd04d6a04c4cef
amit goel <amit.goel@wildhealth.com>
2023-12-06 21:09:23 -0500

    initialize koin on android

--- composeApp/src/androidMain/kotlin/com/ghn/poker/tracker/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/ghn/poker/tracker/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.ghn.poker.tracker
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -8,15 +9,20 @@ import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.ghn.poker.tracker.di.initKoin
 import com.ghn.poker.tracker.ui.App
 import com.ghn.poker.tracker.ui.shared.PrimaryButton
 import com.ghn.poker.tracker.ui.theme.Dimens
 import com.ghn.poker.tracker.ui.theme.GizmoTheme
+import org.koin.dsl.module
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        initKoin(
+            appModule = { module { single<Context> { this@MainActivity.application } } },
+        )
         setContent {
             App()
         }
