c5d136ce12da25e8c0b73adf51d6bf3f75382a14
amit goel <amit.goel@wildhealth.com>
2024-01-06 10:14:05 -0500

    Update desktop config to support webview

--- composeApp/src/desktopMain/kotlin/main.kt
+++ composeApp/src/desktopMain/kotlin/main.kt
@@ -1,17 +1,65 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.runtime.Composable
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import com.ghn.poker.tracker.di.initKoin
 import com.ghn.poker.tracker.ui.App
+import dev.datlag.kcef.KCEF
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import org.koin.dsl.module
+import java.io.File
+import kotlin.math.max
 
 fun main() = application {
+    initKoin {
+        module {}
+    }
+
     Window(onCloseRequest = ::exitApplication) {
-        initKoin {
-            module {}
+        var restartRequired by remember { mutableStateOf(false) }
+        var downloading by remember { mutableStateOf(0F) }
+        var initialized by remember { mutableStateOf(false) }
+
+        LaunchedEffect(Unit) {
+            withContext(Dispatchers.IO) {
+                KCEF.init(builder = {
+                    installDir(File("kcef-bundle"))
+                    progress {
+                        onDownloading {
+                            downloading = max(it, 0F)
+                        }
+                        onInitialized {
+                            initialized = true
+                        }
+                    }
+                    settings {
+                        cachePath = File("cache").absolutePath
+                    }
+                }, onError = {
+                    it?.printStackTrace()
+                }, onRestartRequired = {
+                    restartRequired = true
+                })
+            }
         }
-        App()
+
+        if (restartRequired) {
+            Text(text = "Restart required.")
+        } else {
+            if (initialized) {
+               App()
+            } else {
+                Text(text = "Downloading $downloading%")
+            }
+        }
+
+        DisposableEffect(Unit) {
+            onDispose {
+                KCEF.disposeBlocking()
+            }
+        }
     }
 }
 
