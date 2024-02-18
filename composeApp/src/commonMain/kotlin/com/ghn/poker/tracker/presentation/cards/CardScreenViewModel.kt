a38096ebc96793e35637c6fd5e0f107115df4077
amit goel <amit.goel@wildhealth.com>
2024-02-18 18:43:34 -0500

    Update CardScreenViewModel and presentation logic for 5 card game

--- composeApp/src/commonMain/kotlin/com/ghn/poker/tracker/presentation/cards/CardScreenViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/ghn/poker/tracker/presentation/cards/CardScreenViewModel.kt
@@ -1,15 +1,21 @@
 package com.ghn.poker.tracker.presentation.cards
 
 import co.touchlab.kermit.Logger
+import com.ghn.gizmodb.evaluator.models.Evaluator
+import com.ghn.gizmodb.evaluator.models.getHandRankName
 import com.ghn.poker.tracker.domain.model.Card
 import com.ghn.poker.tracker.domain.model.Deck
+import com.ghn.poker.tracker.domain.usecase.impl.convertCardsToIds
 import com.ghn.poker.tracker.presentation.BaseViewModel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import kotlin.time.DurationUnit
+import kotlin.time.measureTime
 
 class CardScreenViewModel : BaseViewModel() {
     private val deck: List<CardState> = Deck.cards.map { CardState(it, false) }
@@ -28,27 +34,47 @@ class CardScreenViewModel : BaseViewModel() {
                 .collect { action ->
                     when (action) {
                         CardScreenActions.Init -> distributeCards()
-                        CardScreenActions.NewGame -> distributeCards()
+                        CardScreenActions.NewGame -> {
+                            delay(500)
+                            distributeCards()
+                        }
                     }
                 }
         }
     }
 
-    private fun distributeCards() {
+    private suspend fun distributeCards() {
         val shuffled = deck.shuffled()
-        shuffled.onEachIndexed { index, cardState ->
-            Logger.d { "index: $index, Card: ${cardState.card}" }
-        }
         val playerCards = (0 until (state.value.players)).map { playerIndex ->
             (0 until (5)).map { playerCardNumber ->
-
                 shuffled[playerIndex + (state.value.players * playerCardNumber)].card
-            }.also {
-                Logger.d { "Card for player: $playerIndex, $it" }
             }
         }
 
         _state.update { it.copy(playerCard = playerCards) }
+
+        var winnerInfo = WinnerInfo(-1, "")
+        val time = measureTime {
+            playerCards.forEachIndexed { index, cards ->
+                val timeInner = measureTime {
+                    val evaluator = Evaluator()
+                    val rank = evaluator.evaluateCards(cards.convertCardsToIds())
+
+                    if (rank < winnerInfo.winningHandValue) {
+                        Logger.d { "player: ${index + 1} has high hand" }
+                        winnerInfo = winnerInfo.copy(
+                            winnerIndex = index,
+                            winningHandValue = rank,
+                            winningHand = rank.getHandRankName
+                        )
+                    }
+                }
+                Logger.d { "player: ${index + 1} eval finished in ${timeInner.toDouble(DurationUnit.MILLISECONDS)}" }
+            }
+            Logger.d { "Player${winnerInfo.winnerIndex + 1} has the high rank: ${winnerInfo.winningHand}" }
+        }
+        Logger.d { "Total Time to evaluate ${time.toDouble(DurationUnit.MILLISECONDS)}" }
+        _state.update { it.copy(winnerInfo = winnerInfo) }
     }
 
     fun dispatch(action: CardScreenActions) {
@@ -60,6 +86,13 @@ data class CardScreenViewState(
     val item: String = "",
     val players: Int = 9,
     val playerCard: List<List<Card>> = emptyList(),
+    val winnerInfo: WinnerInfo? = null,
+)
+
+data class WinnerInfo(
+    val winnerIndex: Int,
+    val winningHand: String,
+    val winningHandValue: Short = Short.MAX_VALUE
 )
 
 sealed interface CardScreenActions {
